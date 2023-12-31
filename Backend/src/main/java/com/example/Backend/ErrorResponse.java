d37fd2a33c17c2fe8e8ecb95b1ec6c2018555549
michaelteshome1626 <michaelteshome1626@gmail.com>
2023-12-30 23:30:51 -0500

    Creation of RestaurantController File
    Contains methods for handling requests to GET a collection to restaurants or to get a single restaurant.

---
+++ Backend/src/main/java/com/example/Backend/ErrorResponse.java
@@ -0,0 +1,37 @@
+package com.example.Backend;
+
+public class ErrorResponse {
+
+    private String message;
+
+    private String description;
+
+    public ErrorResponse(String message, String description) {
+        this.message = message;
+        this.description = description;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorResponse{" +
+                "message='" + message + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
