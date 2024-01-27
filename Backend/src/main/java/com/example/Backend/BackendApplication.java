9e8d7d43029dc6aa1d9a1f8cd1789fb7aeae1078
AkashSivapalan <akashsivapalan@gmail.com>
2024-01-27 17:04:23 -0500

    Procfile update

--- Backend/src/main/java/com/example/Backend/BackendApplication.java
+++ Backend/src/main/java/com/example/Backend/BackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
